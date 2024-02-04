cc0abdc13bbb10302c948fac4f1b19cdf9587c43
Marcelo Zampietro <58446697+Marcelo-Zampietro@users.noreply.github.com>
2024-02-04 17:54:54 -0500

    prize button script.js

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ const handleYesClick = () => {
 
   // Create and style a new button for Let's Go!
   const letsGoBtn = document.createElement("button");
-  letsGoBtn.textContent = "CLICK FOR PRIZE!!";
+  letsGoBtn.textContent = "CLAIM PRIZE!!";
   letsGoBtn.classList.add("letsgo-btn"); // You can add a class for styling if needed
   letsGoBtn.style.position = "absolute";
 
